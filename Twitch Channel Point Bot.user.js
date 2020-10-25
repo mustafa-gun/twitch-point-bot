85de7aa606044afa23c6b793217061e0445832b6
mustafa-gun <62945169+mustafa-gun@users.noreply.github.com>
2020-10-25 21:16:45 +0300

    Create Twitch Channel Point Bot.user.js

---
+++ Twitch Channel Point Bot.user.js
@@ -0,0 +1,111 @@
+// ==UserScript==
+// @name         TW Point Clicker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Kill Master
+
+// @match        https://www.twitch.tv/*
+// @match        https://twitch.tv/*
+
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+
+// @grant        none
+// ==/UserScript==
+
+
+/*window.onload = function() {
+  document.getElementsByClassName("tw-textarea--no-resize");
+};
+
+
+waitForKeyElements( 'textarea.tw-textarea--no-resize', function(bWaitOnce) {
+    //var e = evetn.keyCode === 13;
+    var c = document.getElementsByClassName("tw-textarea--no-resize");
+    c[0].value = 'ennyoaSa';
+} );*/
+
+waitForKeyElements( 'button.tw-button--success', function() {
+    var x = document.getElementsByClassName("tw-button--success");
+    x[0].click();
+} );
+
+
+
+function waitForKeyElements (
+    selectorTxt,/* Required: The jQuery selector string that
+                        specifies the desired element(s).
+                    */
+    actionFunction, /* Required: The code to run when elements are
+                        found. It is passed a jNode to the matched
+                        element.
+                    */
+    bWaitOnce,/* Optional: If false, will continue to scan for
+                        new elements even after the first match is
+                        found.
+                    */
+    iframeSelector/* Optional: If set, identifies the iframe to
+                        search.
+                    */
+) {
+    var targetNodes, btargetsFound;
+
+if (typeof iframeSelector == "undefined")
+targetNodes     = $(selectorTxt);
+else
+targetNodes     = $(iframeSelector).contents ()
+                                           .find (selectorTxt);
+
+if (targetNodes  &&  targetNodes.length > 0) {
+        btargetsFound   = true;
+        /*--- Found target node(s).  Go through each and act if they
+            are new.
+        */
+        targetNodes.each ( function () {
+            var jThis        = $(this);
+            var alreadyFound = jThis.data ('alreadyFound')  ||  false;
+
+            if (!alreadyFound) {
+                //--- Call the payload function.
+                var cancelFound     = actionFunction (jThis);
+                if (cancelFound)
+                    btargetsFound   = false;
+                else
+                    jThis.data ('alreadyFound', true);
+            }
+        } );
+    }
+    else {
+        btargetsFound   = false;
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj      = waitForKeyElements.controlObj  ||  {};
+    var controlKey      = selectorTxt.replace (/[^\w]/g, "_");
+    var timeControl     = controlObj [controlKey];
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound  &&  bWaitOnce  &&  timeControl) {
+        //--- The only condition where we need to clear the timer.
+        clearInterval (timeControl);
+        delete controlObj [controlKey]
+    }
+    else {
+        //--- Set a timer, if needed.
+        if ( ! timeControl) {
+            timeControl = setInterval ( function () {
+                    waitForKeyElements (    selectorTxt,
+                                            actionFunction,
+                                            bWaitOnce,
+                                            iframeSelector
+                                        );
+                },
+                300
+            );
+            controlObj [controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj   = controlObj;
+}
